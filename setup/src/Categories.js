716022be64fca0ab729e5680c4d110007d74081e
Orhun Alp Kuyucu <112889909+orhun-alp@users.noreply.github.com>
2022-10-16 23:55:29 +0300

    ReactJs-Menu
    ReactJS ile yemek menüsü

---
+++ setup/src/Categories.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Categories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button className="filter-btn" onClick={() => 
+        filterItems("all")}>
+        all
+      </button>
+      <button className="filter-btn" onClick={() => 
+        filterItems("breakfast")}>
+        breakfast
+      </button>
+      <button className="filter-btn" onClick={() => 
+        filterItems("lunch")}>
+        lunch
+      </button>
+      <button className="filter-btn" onClick={() => 
+        filterItems("shakes")}>
+        shakes
+      </button>
+    </div>
+  );
+};
+
+export default Categories;
